fa32c45bd2df0ef1679f566b7e72dfcd6cfadd67
kuriyantoadi <melatikuning212@gmail.com>
2019-12-26 04:41:57 +0700

    update database 26 desember 2019

--- link-net.sql
+++ link-net.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 17, 2019 at 05:00 
+-- Generation Time: Dec 25, 2019 at 10:41 
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -39,8 +39,33 @@ CREATE TABLE IF NOT EXISTS `node` (
 --
 
 INSERT INTO `node` (`regional`, `hub_name`, `node_id`, `node_des`, `titik_ko`) VALUES
-('', 'Diki Saputra', '001', 'ABC', ''),
-('Serang', 'Diki Saputra', '001', 'ABC', '100');
+('001', '001', '001', '001', '001'),
+('002', '002', '002', '002', '002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pimpinan`
+--
+
+CREATE TABLE IF NOT EXISTS `pimpinan` (
+  `node_id` varchar(255) NOT NULL,
+  `node_des` varchar(255) NOT NULL,
+  `hub_name` varchar(255) NOT NULL,
+  `optical_light_rx` varchar(255) NOT NULL,
+  `optical_input_node` varchar(255) NOT NULL,
+  `tgl_lap` varchar(20) NOT NULL,
+  `jam_lap` varchar(20) NOT NULL,
+  `kondisi` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pimpinan`
+--
+
+INSERT INTO `pimpinan` (`node_id`, `node_des`, `hub_name`, `optical_light_rx`, `optical_input_node`, `tgl_lap`, `jam_lap`, `kondisi`) VALUES
+('001', '001', '001', '1', '1', '25-12-2019', '02:38:22', 'Approve'),
+('002', '002', '002', '1', '002', '25-12-2019', '02:45:03', 'Approve');
 
 -- --------------------------------------------------------
 
@@ -53,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `t_coaxial` (
   `node_des` varchar(255) NOT NULL,
   `regional` varchar(255) NOT NULL,
   `hub_name` varchar(255) NOT NULL,
-  `optical_rx` varchar(255) NOT NULL,
+  `optical_input_node` varchar(255) NOT NULL,
   `optical_ac` varchar(255) NOT NULL,
   `optical_dc` varchar(255) NOT NULL,
   `signal_input_low` varchar(255) NOT NULL,
@@ -75,10 +100,9 @@ CREATE TABLE IF NOT EXISTS `t_coaxial` (
 -- Dumping data for table `t_coaxial`
 --
 
-INSERT INTO `t_coaxial` (`node_id`, `node_des`, `regional`, `hub_name`, `optical_rx`, `optical_ac`, `optical_dc`, `signal_input_low`, `signal_input_high`, `signal_output_low`, `signal_output_high`, `signal_output_rev`, `docsis_dp`, `docsis_up`, `docsis_ds`, `quality_ber`, `quality_mer`, `quality_cn`, `pic`, `kondisi`) VALUES
-('001', 'ABC', 'Serang', 'Diki Saputra', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Form ditolak'),
-('1234', 'tidak ada', 'Serang', 'diki', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Form ditolak'),
-('S1', 'ko', 'Jepara', 'Diki', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Menunggu Konfirmasi');
+INSERT INTO `t_coaxial` (`node_id`, `node_des`, `regional`, `hub_name`, `optical_input_node`, `optical_ac`, `optical_dc`, `signal_input_low`, `signal_input_high`, `signal_output_low`, `signal_output_high`, `signal_output_rev`, `docsis_dp`, `docsis_up`, `docsis_ds`, `quality_ber`, `quality_mer`, `quality_cn`, `pic`, `kondisi`) VALUES
+('001', '001', '001', '001', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Sudah dikonfirmasi'),
+('002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', '002', 'Sudah dikonfirmasi');
 
 -- --------------------------------------------------------
 
@@ -87,13 +111,13 @@ INSERT INTO `t_coaxial` (`node_id`, `node_des`, `regional`, `hub_name`, `optical
 --
 
 CREATE TABLE IF NOT EXISTS `t_fo` (
+  `tgl` varchar(15) NOT NULL,
   `node_id` varchar(255) NOT NULL,
   `node_des` varchar(255) NOT NULL,
   `regional` varchar(255) NOT NULL,
   `hub_name` varchar(255) NOT NULL,
   `rack` varchar(255) NOT NULL,
-  `link_1` varchar(255) NOT NULL,
-  `link_2` varchar(255) NOT NULL,
+  `jarak_normal` varchar(255) NOT NULL,
   `pic` varchar(255) NOT NULL,
   `kondisi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -102,29 +126,44 @@ CREATE TABLE IF NOT EXISTS `t_fo` (
 -- Dumping data for table `t_fo`
 --
 
-INSERT INTO `t_fo` (`node_id`, `node_des`, `regional`, `hub_name`, `rack`, `link_1`, `link_2`, `pic`, `kondisi`) VALUES
-('001', 'ABC', 'Serang', 'Diki Saputra', '1', 'Serang', 'Serang', '50', 'Sudah dikonfirmasi');
+INSERT INTO `t_fo` (`tgl`, `node_id`, `node_des`, `regional`, `hub_name`, `rack`, `jarak_normal`, `pic`, `kondisi`) VALUES
+('25-12-2019', '001', '001', '001', '001', '12', '1', '1', 'setuju'),
+('25-12-2019', '002', '002', '002', '002', '002', '002', '1', 'setuju');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_lap`
+-- Table structure for table `t_hub`
 --
 
-CREATE TABLE IF NOT EXISTS `t_lap` (
-  `hub_name` varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `t_hub` (
   `node_id` varchar(255) NOT NULL,
   `node_des` varchar(255) NOT NULL,
-  `optical_rx` varchar(255) NOT NULL,
-  `optical_dc` varchar(255) NOT NULL
+  `regional` varchar(255) NOT NULL,
+  `hub_name` varchar(255) NOT NULL,
+  `optical_light_tx` varchar(255) NOT NULL,
+  `optical_light_rx` varchar(255) NOT NULL,
+  `receiver_no` varchar(255) NOT NULL,
+  `receiver_loss` varchar(255) NOT NULL,
+  `transmitter_no` varchar(255) NOT NULL,
+  `transmitter_persen` varchar(255) NOT NULL,
+  `signal_trans_low` varchar(255) NOT NULL,
+  `signal_trans_high` varchar(255) NOT NULL,
+  `signal_trans_dl` varchar(255) NOT NULL,
+  `cmts_no` varchar(255) NOT NULL,
+  `cmts_ds` varchar(255) NOT NULL,
+  `cmts_us` varchar(255) NOT NULL,
+  `pic` varchar(255) NOT NULL,
+  `kondisi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `t_lap`
+-- Dumping data for table `t_hub`
 --
 
-INSERT INTO `t_lap` (`hub_name`, `node_id`, `node_des`, `optical_rx`, `optical_dc`) VALUES
-('Diki', 'S1', 'ko', '1', '1');
+INSERT INTO `t_hub` (`node_id`, `node_des`, `regional`, `hub_name`, `optical_light_tx`, `optical_light_rx`, `receiver_no`, `receiver_loss`, `transmitter_no`, `transmitter_persen`, `signal_trans_low`, `signal_trans_high`, `signal_trans_dl`, `cmts_no`, `cmts_ds`, `cmts_us`, `pic`, `kondisi`) VALUES
+('001', '001', '001', '001', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Sudah di Input'),
+('002', '002', '002', '002', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 'Sudah di Input');
 
 -- --------------------------------------------------------
 
@@ -161,6 +200,12 @@ ALTER TABLE `node`
   ADD PRIMARY KEY (`regional`);
 
 --
+-- Indexes for table `pimpinan`
+--
+ALTER TABLE `pimpinan`
+  ADD PRIMARY KEY (`node_id`);
+
+--
 -- Indexes for table `t_coaxial`
 --
 ALTER TABLE `t_coaxial`
@@ -173,9 +218,9 @@ ALTER TABLE `t_fo`
   ADD PRIMARY KEY (`node_id`);
 
 --
--- Indexes for table `t_lap`
+-- Indexes for table `t_hub`
 --
-ALTER TABLE `t_lap`
+ALTER TABLE `t_hub`
   ADD PRIMARY KEY (`node_id`);
 
 --
